3030522807dc2eda17657682eb9320b7610cf624
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-23 15:56:05 -0700

    Problem 59.

--- problem.clj
+++ problem.clj
@@ -1,6 +1,5 @@
-(= [3 2 1] ((__ rest reverse) [1 2 3 4]))
-(= 5 ((__ (partial + 3) second) [1 2 3 4]))
-(= true ((__ zero? #(mod % 8) +) 3 5 7 9))
-(= "HELLO" ((__ #(.toUpperCase %) #(apply str %) take) 5 "hello world"))
+(= [21 6 1] ((__ + max min) 2 3 5 1 6 4))
+(= ["HELLO" 5] ((__ #(.toUpperCase %) count) "hello"))
+(= [2 6 4] ((__ :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
 
-(fn [& args] (reduce #(fn [& _args] (%2 (apply %1 _args))) (reverse args)))
+(fn [& fs] (fn [& args] (for [f fs] (apply f args))))
