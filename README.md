e72d27cffb795176f44c1ecbeee3b4aefd903f77
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-23 10:34:12 -0700

    Baseline

---
+++ README.md
@@ -0,0 +1,12 @@
+# Usage
+
+1. Start up a REPL. E.g. `lein repl`
+1. Edit `problem.clj` to include the (4clojure problem)[https://github.com/4clojure/4clojure/blob/develop/src/foreclojure/data_set.clj] you want to work on.
+2. Edit `problem.clj` to include your desired substitution for the solution.
+3. Execute `(load-file "test.clj")` on the REPL.
+4. Repeat at will (from step 2 or 1).
+
+# Contributions
+
+Contributions are welcome. Open a pull request!
+
---
+++ test.clj
@@ -0,0 +1,14 @@
+(let [lines     (clojure.string/split-lines (slurp "problem.clj"))
+			separator (.indexOf lines "")
+			tests     (take (dec separator) lines)
+			solution  (clojure.string/join "\n" (drop (inc separator) lines))]
+	(doseq [test tests]
+		(let [code         (clojure.string/replace test "__" solution)
+			    form         (read-string code)
+			    result       (eval form)
+			    intermediary (eval (second form))]
+			(prn test)
+			(print "=> ")
+			(prn intermediary)
+			(prn result)
+			(println))))
