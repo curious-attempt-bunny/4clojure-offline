e810764984a818f352aca3fdbc468b0581fe50ed
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-23 15:50:24 -0700

    Problem 58.

--- problem.clj
+++ problem.clj
@@ -1,6 +1,6 @@
-(= (__ [1 2 1 3 1 2 4]) [1 2 3 4])
-(= (__ [:a :a :b :b :c :c]) [:a :b :c])
-(= (__ '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3]))
-(= (__ (range 50)) (range 50))
+(= [3 2 1] ((__ rest reverse) [1 2 3 4]))
+(= 5 ((__ (partial + 3) second) [1 2 3 4]))
+(= true ((__ zero? #(mod % 8) +) 3 5 7 9))
+(= "HELLO" ((__ #(.toUpperCase %) #(apply str %) take) 5 "hello world"))
 
-reduce (fn [coll x] (if (some #(= % x) coll) coll (conj coll x))) []
+(fn [& args] (reduce #(fn [& _args] (%2 (apply %1 _args))) (reverse args)))
