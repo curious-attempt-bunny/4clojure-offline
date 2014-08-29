eddb5f533e22e68b3a12c9733763165ad3023078
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-29 11:00:05 -0700

    Extract method.

--- update.clj
+++ update.clj
@@ -1,13 +1,14 @@
 (defn directory [problem] (str "problems/" (first (:tags problem))))
+(defn path [problem]
+	(let [id         (format "%02d" (:_id problem))
+				identifier (clojure.string/replace (clojure.string/lower-case (:title problem)) #"[^a-z]+" "_")
+				filename   (str id "_" identifier ".clj")]
+			 (str (directory problem) "/" filename)))
 
 (defn insert! [type problem]
 	(if (true? (:approved problem))
 			(do
-				(let [id         (format "%02d" (:_id problem))
-							identifier (clojure.string/replace (clojure.string/lower-case (:title problem)) #"[^a-z]+" "_")
-							filename   (str id "_" identifier ".clj")
-							path       (str (directory problem) "/" filename)
-							url        (str "http://www.4clojure.com/problem/" (:_id problem))
+				(let [url        (str "http://www.4clojure.com/problem/" (:_id problem))
 							contents   (concat [(str "; " (:description problem))]
 																 [(str "; " url)]
 																 (if (:restricted problem)
@@ -18,7 +19,7 @@
 																 [""]
 																 [""])]
 							(.mkdir (java.io.File. (directory problem)))
-							(if-not (.exists (clojure.java.io/file path))
+							(if-not (.exists (clojure.java.io/file (path problem)))
 								(spit path (clojure.string/join "\n" contents)))
 				))))
 (eval (read-string (clojure.string/join "\n" (drop 3 (clojure.string/split (slurp "data_set.clj") #"\n")))))
