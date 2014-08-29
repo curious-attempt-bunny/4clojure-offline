34fedde122537a5fd3e08473aeab1e0caa05357b
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-29 11:38:55 -0700

    Inline test.clj.

--- update.clj
+++ update.clj
@@ -1,29 +1,53 @@
+(use 'clojure.string)
+
 (defn directory [problem] (str "problems/" (first (:tags problem))))
 
 (defn path [problem]
 	(let [id         (format "%02d" (:_id problem))
-				identifier (clojure.string/replace (clojure.string/lower-case (:title problem)) #"[^a-z]+" "_")
+				identifier (replace (lower-case (:title problem)) #"[^a-z]+" "_")
 				filename   (str id "_" identifier ".clj")]
 			 (str (directory problem) "/" filename)))
 (defn url [problem] (str "http://www.4clojure.com/problem/" (:_id problem)))
 
 (defn base-content [problem]
-	(concat [(str "; " (:description problem))]
+	(concat [(str "; " (replace (:description problem) #"</?[a-z]+/?>|\n" ""))]
 					[(str "; " (url problem))]
 					(if (:restricted problem)
-							 [(str "; Don't use: " (clojure.string/join ", nor " (:restricted problem)))]
+							 [(str "; Don't use: " (join ", nor " (:restricted problem)))]
 							 [])
 					[""]
-					(map #(str "; " (clojure.string/replace % #"\n" " ") ) (:tests problem))
+					(map #(str "; " (replace % #"\n" " ") ) (:tests problem))
 					[""]
 					[""]))
 
 (defn test-problem [problem]
-	; TODO
-	)
+	(let [lines     (split-lines (slurp (path problem)))
+				solution  (trim (join "\n" (filter #(not= (first %) \;) lines)))]
+		(if-not (blank? solution)
+			(do 
+				(println (path problem))
+				(doseq [test (:tests problem)]
+					(let [code         (replace test "__" solution)
+						    form         (read-string code)
+						    result       (eval form)
+						    inspectcode  (replace test "__" "(throw (Exception. \"correct clause!\"))")
+						    subtitutionindex (try (do
+						    												(eval (second (read-string inspectcode)))
+						    												2)
+						    											(catch Exception e (if (= "correct clause!" (.getMessage e))
+						    																						 1
+						    																						 2)))
+						    intermediary (eval (nth form subtitutionindex))]
+
+						(prn solution)
+						(prn test)
+						(print "=> ")
+						(prn intermediary)
+						(prn result)
+						(println)))))))
 
 (defn write-problem [problem]
-	(spit (path problem) (clojure.string/join "\n" (base-content problem))))
+	(spit (path problem) (join "\n" (base-content problem))))
 
 (defn insert! [type problem]
 	(if (true? (:approved problem))
@@ -35,5 +59,5 @@
 				))))
 
 (.mkdir (java.io.File. "problems"))
-(eval (read-string (clojure.string/join "\n" (drop 3 (clojure.string/split (slurp "data_set.clj") #"\n")))))
+(eval (read-string (join "\n" (drop 3 (split (slurp "data_set.clj") #"\n")))))
 (load-problems)
