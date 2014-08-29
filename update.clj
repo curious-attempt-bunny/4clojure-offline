c6080aacca7090a2f24764b05479c0249b1e0b46
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-29 15:34:09 -0700

    Track hashes of evaluated problems.

--- update.clj
+++ update.clj
@@ -1,5 +1,7 @@
 (use 'clojure.string)
 
+(.mkdir (java.io.File. "problems"))
+
 (defn directory [problem] (str "problems/" (first (:tags problem))))
 
 (defn path [problem]
@@ -20,31 +22,48 @@
 					[""]
 					[""]))
 
+(defn test-expression [solution test]
+	(let [code         (replace test "__" solution)
+		    form         (read-string code)
+		    result       (eval form)
+		    inspectcode  (replace test "__" "(throw (Exception. \"correct clause!\"))")
+		    subtitutionindex (try (do
+		    												(eval (second (read-string inspectcode)))
+		    												2)
+		    											(catch Exception e (if (= "correct clause!" (.getMessage e))
+		    																						 1
+		    																						 2)))
+		    intermediary (eval (nth form subtitutionindex))]
+		(if result
+			{:success true}
+			{:success false :test test :intermediary intermediary})))
+
+(spit "problems/state.csv" "" :append true)
+
+(def problem-hashes-evaluated
+	(let [csv (filter #(> (count %) 1) (map #(split % #",") (split (slurp "problems/state.csv") #"\n")))]
+		(reduce #(assoc %1 (Integer/parseInt (first %2)) (Integer/parseInt (second %2))) {} csv)))
+
+(defn append-result [id hash success] (spit "problems/state.csv" (str id "," hash "," success "\n") :append true))
+
 (defn test-problem [problem]
 	(let [lines     (split-lines (slurp (path problem)))
 				solution  (trim (join "\n" (filter #(not= (first %) \;) lines)))]
 		(if-not (blank? solution)
-			(do 
-				(println (path problem))
-				(doseq [test (:tests problem)]
-					(let [code         (replace test "__" solution)
-						    form         (read-string code)
-						    result       (eval form)
-						    inspectcode  (replace test "__" "(throw (Exception. \"correct clause!\"))")
-						    subtitutionindex (try (do
-						    												(eval (second (read-string inspectcode)))
-						    												2)
-						    											(catch Exception e (if (= "correct clause!" (.getMessage e))
-						    																						 1
-						    																						 2)))
-						    intermediary (eval (nth form subtitutionindex))]
-
-						(prn solution)
-						(prn test)
-						(print "=> ")
-						(prn intermediary)
-						(prn result)
-						(println)))))))
+			(if-not (= (hash solution) (problem-hashes-evaluated (:_id problem)))
+				(let [results (map #(test-expression solution %) (:tests problem))
+							success (every? #(:success %) results)]
+					(append-result (:_id problem) (hash solution) success)
+					(print (str "#" (:_id problem) " - " (:title problem) ": "))
+					(if success
+						(println "complete!")
+						(do
+							(println)
+							(doseq [result results]
+								(prn (:test result))
+								(print " => ")
+								(prn (:intermediary result))
+								(println)))))))))
 
 (defn write-problem [problem]
 	(spit (path problem) (join "\n" (base-content problem))))
@@ -58,6 +77,6 @@
 					(write-problem problem)
 				))))
 
-(.mkdir (java.io.File. "problems"))
 (eval (read-string (join "\n" (drop 3 (split (slurp "data_set.clj") #"\n")))))
+
 (load-problems)
