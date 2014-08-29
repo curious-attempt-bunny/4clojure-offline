92e6f5e10322155b585b63f29f230c60255dad93
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-29 15:40:15 -0700

    Fix bug the hid clojure.core/reverse.

--- update.clj
+++ update.clj
@@ -1,21 +1,19 @@
-(use 'clojure.string)
-
 (.mkdir (java.io.File. "problems"))
 
 (defn directory [problem] (str "problems/" (first (:tags problem))))
 
 (defn path [problem]
 	(let [id         (format "%02d" (:_id problem))
-				identifier (replace (lower-case (:title problem)) #"[^a-z]+" "_")
+				identifier (clojure.string/replace (clojure.string/lower-case (:title problem)) #"[^a-z]+" "_")
 				filename   (str id "_" identifier ".clj")]
 			 (str (directory problem) "/" filename)))
 (defn url [problem] (str "http://www.4clojure.com/problem/" (:_id problem)))
 
 (defn base-content [problem]
-	(concat [(str "; " (replace (:description problem) #"</?[a-z]+/?>|\n" ""))]
+	(concat [(str "; " (clojure.string/replace (:description problem) #"</?[a-z]+/?>|\n" ""))]
 					[(str "; " (url problem))]
 					(if (:restricted problem)
-							 [(str "; Don't use: " (join ", nor " (:restricted problem)))]
+							 [(str "; Don't use: " (clojure.string/join ", nor " (:restricted problem)))]
 							 [])
 					[""]
 					(map #(str "; " (replace % #"\n" " ") ) (:tests problem))
@@ -23,10 +21,10 @@
 					[""]))
 
 (defn test-expression [solution test]
-	(let [code         (replace test "__" solution)
+	(let [code         (clojure.string/replace test "__" solution)
 		    form         (read-string code)
 		    result       (eval form)
-		    inspectcode  (replace test "__" "(throw (Exception. \"correct clause!\"))")
+		    inspectcode  (clojure.string/replace test "__" "(throw (Exception. \"correct clause!\"))")
 		    subtitutionindex (try (do
 		    												(eval (second (read-string inspectcode)))
 		    												2)
@@ -41,15 +39,15 @@
 (spit "problems/state.csv" "" :append true)
 
 (def problem-hashes-evaluated
-	(let [csv (filter #(> (count %) 1) (map #(split % #",") (split (slurp "problems/state.csv") #"\n")))]
+	(let [csv (filter #(> (count %) 1) (map #(clojure.string/split % #",") (clojure.string/split (slurp "problems/state.csv") #"\n")))]
 		(reduce #(assoc %1 (Integer/parseInt (first %2)) (Integer/parseInt (second %2))) {} csv)))
 
 (defn append-result [id hash success] (spit "problems/state.csv" (str id "," hash "," success "\n") :append true))
 
 (defn test-problem [problem]
-	(let [lines     (split-lines (slurp (path problem)))
-				solution  (trim (join "\n" (filter #(not= (first %) \;) lines)))]
-		(if-not (blank? solution)
+	(let [lines     (clojure.string/split-lines (slurp (path problem)))
+				solution  (clojure.string/trim (clojure.string/join "\n" (filter #(not= (first %) \;) lines)))]
+		(if-not (clojure.string/blank? solution)
 			(if-not (= (hash solution) (problem-hashes-evaluated (:_id problem)))
 				(let [results (map #(test-expression solution %) (:tests problem))
 							success (every? #(:success %) results)]
@@ -66,7 +64,7 @@
 								(println)))))))))
 
 (defn write-problem [problem]
-	(spit (path problem) (join "\n" (base-content problem))))
+	(spit (path problem) (clojure.string/join "\n" (base-content problem))))
 
 (defn insert! [type problem]
 	(if (true? (:approved problem))
@@ -77,6 +75,6 @@
 					(write-problem problem)
 				))))
 
-(eval (read-string (join "\n" (drop 3 (split (slurp "data_set.clj") #"\n")))))
+(eval (read-string (clojure.string/join "\n" (drop 3 (clojure.string/split (slurp "data_set.clj") #"\n")))))
 
 (load-problems)
