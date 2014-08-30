5a0e95b8d08d0162e3a223be6fc95d2e3205f3bb
Merlyn <curious.attempt.bunny@gmail.com>
2014-08-30 10:17:42 -0700

    Solved some elementary problems.

--- problems/elementary/68_recurring_theme.clj
+++ problems/elementary/68_recurring_theme.clj
@@ -3,3 +3,4 @@
 
 ; (= __   (loop [x 5          result []]     (if (> x 0)       (recur (dec x) (conj result (+ 2 x)))       result)))
 
+[7 6 5 4 3]
--- problems/elementary/71_rearranging_code_.clj
+++ problems/elementary/71_rearranging_code_.clj
@@ -3,3 +3,4 @@
 
 ; (= (__ (sort (rest (reverse [2 5 4 1 3 6]))))    (-> [2 5 4 1 3 6] reverse rest sort __)    5)
 
+last
