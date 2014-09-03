eed83be797927a9e02dc57dbde3614579b70df12
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-02 21:37:39 -0700

    Solved easy problem 31.

--- problems/easy/31_pack_a_sequence.clj
+++ problems/easy/31_pack_a_sequence.clj
@@ -5,3 +5,11 @@
 ; (= (__ [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
 ; (= (__ [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
 
+reduce (fn [result elem]
+	(let [current (last result)
+		 		past    (vec (butlast result))]
+		(if (= (first current) elem)
+			(conj past (conj current elem))
+			(conj result (list elem)))))
+	[]
+	
