3a78d2d58d4c0867e10a5525a4e1efa79d0b9e5a
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-03 08:13:15 -0700

    Solved easy problem 41.

--- problems/easy/41_drop_every_nth_item.clj
+++ problems/easy/41_drop_every_nth_item.clj
@@ -5,3 +5,7 @@
 ; (= (__ [:a :b :c :d :e :f] 2) [:a :c :e])
 ; (= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6])
 
+(fn drop-nth [coll n]
+	(if (empty? coll)
+		coll
+		(lazy-cat (take (dec n) coll) (drop-nth (drop n coll) n))))
