23fe814c65929800baa70f13b8cf76822784796f
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-03 11:24:16 -0700

    Solved easy problem 61.

--- problems/easy/61_map_construction.clj
+++ problems/easy/61_map_construction.clj
@@ -6,3 +6,5 @@
 ; (= (__ [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})
 ; (= (__ [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})
 
+(fn [keys values]
+	(apply hash-map (mapcat list keys values)))
