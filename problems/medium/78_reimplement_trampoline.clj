f014abeb924488cea87f0350515e25f7a1d00374
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-05 10:53:52 -0700

    Solved medium problem 78.

--- problems/medium/78_reimplement_trampoline.clj
+++ problems/medium/78_reimplement_trampoline.clj
@@ -5,3 +5,8 @@
 ; (= (letfn [(triple [x] #(sub-two (* 3 x)))           (sub-two [x] #(stop?(- x 2)))           (stop? [x] (if (> x 50) x #(triple x)))]     (__ triple 2))   82)
 ; (= (letfn [(my-even? [x] (if (zero? x) true #(my-odd? (dec x))))           (my-odd? [x] (if (zero? x) false #(my-even? (dec x))))]     (map (partial __ my-even?) (range 6)))   [true false true false true false])
 
+(fn _trampoline [f & args]
+	(let [result (apply f args)]
+		(if (fn? result) 
+			(_trampoline result)
+			result)))
