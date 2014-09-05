402cf959437b6f024b178d2d666be5ffc3d9dd72
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-05 10:10:07 -0700

    Solved medium problem 69.

--- problems/medium/69_merge_with_a_function.clj
+++ problems/medium/69_merge_with_a_function.clj
@@ -6,3 +6,14 @@
 ; (= (__ - {1 10, 2 20} {1 3, 2 10, 3 15})    {1 7, 2 10, 3 15})
 ; (= (__ concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})    {:a [3 4 5], :b [6 7], :c [8 9]})
 
+(fn m 
+	([f] {})
+	([f acc & args]
+		(reduce 
+			(fn [result [k v]]
+				(assoc result k
+					(if (contains? result k)
+						(f (result k) v)
+						v)))
+			acc
+			(apply m (cons f args)))))
