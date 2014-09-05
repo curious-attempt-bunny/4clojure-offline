7e7af1711b381952cc25a84968f5422e1bf57d3d
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-05 10:53:03 -0700

    Solved medium problem 76.

--- problems/medium/76_intro_to_trampoline.clj
+++ problems/medium/76_intro_to_trampoline.clj
@@ -3,3 +3,4 @@
 
 ; (= __    (letfn      [(foo [x y] #(bar (conj x y) y))       (bar [x y] (if (> (last x) 10)                    x                    #(foo x (+ 2 y))))]      (trampoline foo [] 1)))
 
+[1 3 5 7 9 11]
