2e74cb6c51b13986456fce2e801883276b391313
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-06 11:28:49 -0700

    Solved hard problem 79.

--- problems/hard/79_triangle_minimal_path.clj
+++ problems/hard/79_triangle_minimal_path.clj
@@ -4,3 +4,17 @@
 ; (= (__ [   [1]           [2 4]          [5 1 4]         [2 3 4 5]])    (+ 1 2 1 3)    7)
 ; (= (__ [     [3]             [2 4]            [1 9 3]           [9 9 2 4]          [4 6 6 7 8]         [5 7 3 5 1 4]])    (+ 3 4 3 2 7 1)    20)
 
+(fn solve
+	([triangle] (solve triangle 0 0))
+	([triangle acc index]
+		(if (empty? triangle)
+			acc
+			(let [layer   (first triangle)
+						beneath (rest triangle)
+						val     (nth layer index)
+						base    (+ acc val)
+						left    index
+						right   (inc index)]
+				(min
+					(solve beneath base left)
+					(solve beneath base right))))))
