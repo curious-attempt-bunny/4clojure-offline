817a3cbb855d7c329f1b1b0dcf721e0b615dc2b0
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-08 09:35:38 -0700

    Solved hard problem 82.

--- problems/hard/82_word_chains.clj
+++ problems/hard/82_word_chains.clj
@@ -8,3 +8,32 @@
 ; (= true (__ #{"share" "hares" "shares" "hare" "are"}))
 ; (= false (__ #{"share" "hares" "hare" "are"}))
 
+(fn chainable?
+	([words] (true? (some #(chainable? (disj words %) % []) words)))
+	([words leading route]
+		(if (empty? words)
+			true
+			(some
+				true?
+				(for [other words]
+					(let [remaining  (disj words leading other)
+								diff_index (fn [w1 w2]
+															(loop [a (vec w1)
+																		 b (vec w2)
+																		 i 0]
+																(if (and (empty? a) (empty? a))
+																	i
+																	(if (= (first a) (first b))
+																		(recur (rest a) (rest b) (inc i))
+																		i))))
+								diff_left  (diff_index leading other)
+								diff_right (diff_index (clojure.string/reverse leading) (clojure.string/reverse other))
+								diff_total (+ diff_left diff_right)
+								target     (dec (max (count leading) (count other)))
+								valid?     (= diff_total target)
+								route      (conj route other)]
+						(when valid?
+							(chainable? remaining other route))))))))
+
+				
+			
